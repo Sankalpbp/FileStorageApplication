4f0a7ac2f56443a5f99a19d65858f489b68a9c4d
Sankalpbp <sankalpnitj@gmail.com>
2023-09-05 17:39:56 +0530

    filename validations added for filename and filetype

--- file-storage-service/src/main/java/ai/typeface/filestorageservice/util/CloudStorageUtil.java
+++ file-storage-service/src/main/java/ai/typeface/filestorageservice/util/CloudStorageUtil.java
@@ -1,13 +1,16 @@
 package ai.typeface.filestorageservice.util;
 
+import ai.typeface.filestorageservice.exception.FileMetadataValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public final class CloudStorageUtil {
 
@@ -19,7 +22,19 @@ public final class CloudStorageUtil {
             LOGGER.error ( "An error occurred while reading the file name from the provided file to upload / update: " );
             throw new RuntimeException ( "An error occurred while reading the file name from the provided file to upload / update." );
         }
-        return originalFileName;
+
+        String [] partsOfFileName = originalFileName.split ( "\\." );
+        int numberOfStrings = ( int ) Arrays.stream ( partsOfFileName )
+                                            .filter ( s -> !s.isBlank () )
+                                            .count ();
+
+        if ( numberOfStrings < 2 || ( partsOfFileName [ 0 ].isBlank () || partsOfFileName [ partsOfFileName.length - 1 ].isBlank() ) ) {
+            LOGGER.error ( "An error occurred while reading the file. Malformed filename. Should be of type: name.type" );
+            throw new FileMetadataValidationException ( HttpStatus.BAD_REQUEST,
+                                    "An error occurred while reading the file. Malformed filename" );
+        }
+
+        return partsOfFileName [ partsOfFileName.length - 1 ];
     }
 
     /* TODO: Throw a custom exception from here */
