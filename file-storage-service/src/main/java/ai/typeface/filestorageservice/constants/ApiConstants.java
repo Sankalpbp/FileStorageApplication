8a6541c68ccd7eeb86eb86f7f339b393f51a1195
Sankalpbp <sankalpnitj@gmail.com>
2023-09-06 18:31:20 +0530

    Updated Default sort by field for getAllFiles () api

--- file-storage-service/src/main/java/ai/typeface/filestorageservice/constants/ApiConstants.java
+++ file-storage-service/src/main/java/ai/typeface/filestorageservice/constants/ApiConstants.java
@@ -4,7 +4,7 @@ public final class ApiConstants {
 
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String DEFAULT_PAGE_SIZE = "5";
-    public static final String DEFAULT_SORT_BY_FIELD = "uniqueIdentifier";
+    public static final String DEFAULT_SORT_BY_FIELD = "createdAt";
     public static final String DEFAULT_SORT_DIRECTION = "asc";
 
     public static final String ATTACHMENT_FILENAME_HEADER = "attachment; filename=";
